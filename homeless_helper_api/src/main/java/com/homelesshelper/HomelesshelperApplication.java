ead8240e6962f7b3cfba4a19889d37bf430a67ef
Louis Heath <lheath1998@gmail.com>
2019-01-26 15:32:34 +0000

    init api

---
+++ homeless_helper_api/src/main/java/com/homelesshelper/HomelesshelperApplication.java
@@ -0,0 +1,14 @@
+package com.homelesshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelesshelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelesshelperApplication.class, args);
+	}
+
+}
+
